5fb9db7667c5aa37a6c340782df0873d607da37f
VedantKale08 <fscocomputer@gmail.com>
2024-01-06 11:34:44 +0530

    some socket setup

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
     console.log("Connection established ",socket.id);
+
+      socket.on("join-room", (roomId, id) => {
+        console.log(`A new user ${id} has joined the room ${roomId}`);
+        socket.join(roomId)
+        socket.broadcast.to(roomId).emit("user-connected",id)
+      });
+
 });
 
 httpServer.listen(5000,()=>console.log("Server listening on port 5000"));
